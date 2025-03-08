f6b41501498e8b2f510c0f861c55d3924fe7d6e2
Dimitar <dimi.dimitrow27@gmail.com>
2025-03-08 21:58:45 +0100

    Added blog post data structure and Flask homepage route

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+with open("blogposts.json", "r") as fileobj:
+    books = json.loads(fileobj.read())
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open("blogposts.json", "r") as fileobj:
+        blog_posts = json.loads(fileobj.read())
+
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
